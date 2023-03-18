c8d5d62ec96066bbe274c373307cd3f73872a538
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-18 15:08:31 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-
+import React, {useState} from 'react';
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+const WeatherApp = () => {
+   const [weather, setWeather] = useState({ temperature: 25, conditions: "Sunny" });
+  const temperatureColor = weather.temperature > 20 ? "red" : "blue";
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Current weather conditions:</h2>
+      <p>Temperature: <span style={{ color: temperatureColor }}>{weather.temperature}°C</span></p>
+      <p>Conditions: {weather.conditions}</p>
     </div>
   )
 }
 
-export default App
+export default WeatherApp;
